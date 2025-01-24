f67fea4f1935636c80c829a4be5991d451a42667
kimmartelolives <kimmartel.olives@gmail.com>
2025-01-24 18:48:30 +0800

    added new response

--- script.js
+++ script.js
@@ -83,7 +83,17 @@ const botReplies = {
     "haha": [
         "Happy yarn? Ok yan ganyan lang lagi!",
         "HAHAHAHAHAHAHAHAAHAHA gew",
-        "HGJAKHDJHAGDGHDHGAHHGAHAHAHNVADA"
+        "HGJAKHDJHAGDGHDHGAHHGAH"
+    ],
+    "hahahaha": [
+        "Happy yarn? Ok yan ganyan lang lagi!",
+        "HAHAHAHAHAHAHAHAAHAHA gew",
+        "HGJAKHDJHAGDGHDHGAHHGAH"
+    ],
+    "hahaha": [
+        "Happy yarn? Ok yan ganyan lang lagi!",
+        "HAHAHAHAHAHAHAHAAHAHA gew",
+        "HGJAKHDJHAGDGHDHGAHHGAH"
     ],
     "cute": [
         "Uu parang ikaw no?",
