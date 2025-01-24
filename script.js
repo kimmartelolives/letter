330e500a351e03273e04ec2bbd5c9406ec1a4d9c
kimmartelolives <kimmartel.olives@gmail.com>
2025-01-24 18:47:40 +0800

    added time

--- script.js
+++ script.js
@@ -397,13 +397,16 @@ function botResponse(message) {
     botMessage.classList.add('bot-message');
     botMessage.textContent = message;
 
+    
+
+    messageContainer.appendChild(botMessage);
+    chatLog.appendChild(messageContainer);
+
     const timeDiv = document.createElement('div');
     timeDiv.classList.add('message-time');
     timeDiv.textContent = getCurrentTime();
     messageContainer.appendChild(timeDiv);
-
-    messageContainer.appendChild(botMessage);
-    chatLog.appendChild(messageContainer);
+    
     chatLog.scrollTop = chatLog.scrollHeight; // Scroll to bottom
 }
 
